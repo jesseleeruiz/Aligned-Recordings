bdb810c04b252a65a1ce04865517a6f54ba1c6ee
jesseleeruiz <jesseleeruiz@gmail.com>
2020-04-24 13:07:48 -1000

    Set up MainVC. Added Record and Import Buttons to bottom with Constraints and Accessibility Label and Hint. Added TableVC. Set Delegate and Datasource to MainVC. Stubbed out protocols of TableVC.

--- Aligned Recordings/Aligned Recordings/ViewController.swift
+++ Aligned Recordings/Aligned Recordings/ViewController.swift
@@ -12,9 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
