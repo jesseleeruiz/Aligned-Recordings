6a6303b6db86bf622631775e83d18f1edde1813f
jesseleeruiz <jesseleeruiz@gmail.com>
2020-04-27 07:20:17 -1000

    Set project up with CloudKit. Added TranscriptController File. Seperated files into folders.

---
+++ Aligned Recordings/Aligned Recordings/Model Controller/TranscriptController.swift
@@ -0,0 +1,11 @@
+//
+//  TranscriptController.swift
+//  Aligned Recordings
+//
+//  Created by Jesse Ruiz on 4/27/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
